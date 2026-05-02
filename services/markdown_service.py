f81640cb4c5c8e2f2071fd575250ff23994389e1
Shiva <aniketpatro0412@gmail.com>
2026-05-02 18:32:42 +0530

    Update Markdown Service

--- services/markdown_service.py
+++ services/markdown_service.py
@@ -1,18 +1,43 @@
 from datetime import datetime
 import os
 
+from utils.logger import logger
+
 #Initialising a Markdown file if not already
 def initialize_markdown(file_path: str) -> None:
 
+    try:
+        if not os.path.exists(file_path):
 
-    if not os.path.exists(file_path):
-        with open(file_path, "w") as f:
-            f.write(f"# Daily Q&A Log - {datetime.now().date()}\n\n")
+            with open(file_path, "w") as f:
+                f.write(f"# Daily Q&A Log - {datetime.now().date()}\n\n")
+            logger.info("Markdown file initialized")
+
+    except Exception as e:
+        logger.error(f"Failed to initialize markdown | Error: {e}")
+        raise
+
+    # if not os.path.exists(file_path):
+    #     with open(file_path, "w") as f:
+    #         f.write(f"# Daily Q&A Log - {datetime.now().date()}\n\n")
 
 
 
 def append_qa_to_markdown(file_path: str, question: str, answer: str) -> None:
-    with open(file_path, "a") as f:
-        f.write(f"## Question\n{question}\n\n")
-        f.write(f"### Answer\n{answer}\n\n")
-        f.write("---\n\n")
+
+    try:
+        with open(file_path, "a") as f:
+            f.write(f"## Question\n{question}\n\n")
+            f.write(f"### Answer\n{answer}\n\n")
+            f.write("---\n\n")
+
+        logger.info("Appended Q&A to markdown")
+
+    except Exception as e:
+        logger.error(f"Failed to write markdown | Error: {e}")
+        raise
+
+    # with open(file_path, "a") as f:
+    #     f.write(f"## Question\n{question}\n\n")
+    #     f.write(f"### Answer\n{answer}\n\n")
+    #     f.write("---\n\n")
