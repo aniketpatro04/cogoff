aa55cb7f5b9ff01312c68591847233ecb6b3f894
Shiva <aniketpatro0412@gmail.com>
2026-05-02 18:27:43 +0530

    update Excel Service

--- services/excel_service.py
+++ services/excel_service.py
@@ -1,41 +1,65 @@
 import pandas as pd
 from models.question import Question
+from utils.logger import logger
 
 
 
 # Gettting the List of all the questions
 def load_questions(file_path: str) -> list[Question]:
 
-    df = pd.read_excel(file_path)
+
+    try:
+        df = pd.read_excel(file_path)
 
 
-    questions = []
+        questions = []
 
-    # The iterrows function allows to iterate over a table (dataframe) and returns the tupe containing the index and the data as a series
-    for idx,row in df.iterrows():
+        # The iterrows function allows to iterate over a table (dataframe) and returns the tupe containing the index and the data as a series
+        for idx,row in df.iterrows():
 
-        questions.append(
-            Question(
-                id = idx,
-                text = row.get("Questions" , ""),
-                is_answered=bool(row.get("Answered" , False))
+            question_text = str(row.get("Questions", "")).strip()
+            is_answered = bool(row.get("Answered", False))
+
+            questions.append(
+                Question(
+                    id = idx,
+                    text = question_text,
+                    is_answered=is_answered
+                )
             )
-        )
-
-    return questions
+        
+        logger.info(f"Loaded {len(questions)} questions")
+        return questions
+    
+    except Exception as e:
+        logger.error(f"Failed to load Excel file | Error: {e}")
+        raise
 
 
 # Getting the list of Unanswered questions
 # Using List Comprehension
 def get_unanswered_questions(questions: list[Question]) -> list[Question]:
-    return [q for q in questions if not q.is_answered and q.text.strip()]
+    
+    filtered = [q for q in questions if not q.is_answered and q.text]
+    logger.info(f"Found {len(filtered)} unanswered questions")
+    return filtered
+    #return [q for q in questions if not q.is_answered and q.text.strip()]
 
 
 # Function to mark the question as answered
 def mark_question_as_answered(file_path: str, question_id: int) -> None:
 
 
-    df = pd.read_excel(file_path)
-    df.at[question_id, "Answered"] = True
-    df.to_excel(file_path, index=False)
+    try:
+        df = pd.read_excel(file_path)
+
+        df.at[question_id, "Answered"] = True
+        df.to_excel(file_path, index=False)
+
+        logger.info(f"Marked question {question_id} as answered")
+
+    except Exception as e:
+        
+        logger.error(f"Failed to update Excel | Question ID: {question_id} | Error: {e}")
+        raise
 
