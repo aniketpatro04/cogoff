1dd75a2c3af03b4b5d584efb781e317c88864f79
Shiva <aniketpatro0412@gmail.com>
2026-05-16 11:45:43 +0530

    Add main.py

---
+++ app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Cognitive Offloading API",
+    version="1.0",
+    description="API for processing questions and answers with cognitive offloading"
+)
+
